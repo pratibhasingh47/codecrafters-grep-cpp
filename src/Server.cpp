ada9ead766a46da1b742dcef4ff79fece7168a6f
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-30 11:13:07 +0530

    pass stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,73 +1,60 @@
 #include <iostream>
 #include <string>
-
-bool match_pattern(const std::string& input_line, const std::string& pattern) {
-    if (pattern.length() == 1) {
-        return input_line.find(pattern) != std::string::npos;
+bool match_pattern(const std::string &input_line, const std::string &pattern) {
+  if (pattern.length() == 1) {
+    return input_line.find(pattern) != std::string::npos;
+  } else if (pattern == "\\d") {
+    for (char c : input_line) {
+      if (std::isdigit(c)) {
+        return true;
+      }
     }
-    else if (pattern == "\\d") {
-        return input_line.find_first_of("123456890") != std::string::npos;
+    return false;
+  } else if (pattern == "\\w") {
+    for (char c : input_line) {
+      if (std::isalpha(c)) {
+        return true;
+      }
     }
-    else if (pattern == "\\w") {
-        for (const auto &l : input_line) {
-            if (std::isdigit(l) || std::isalpha(l)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    else if (pattern.at(0) == '[' && pattern.at(pattern.length() - 1) == ']') {
-        bool negate = pattern[1] == '^';
-        std::string chars_to_match =
+    return false;
+  } else if (pattern[0] == '[' && pattern[pattern.size() - 1] == ']') {
+    
+    bool negate = pattern[1] == '^';
+    std::string chars_to_match =
         pattern.substr(negate ? 2 : 1, pattern.size() - (negate ? 3 : 2));
-        for (const auto &c : input_line) {
-            if (chars_to_match.find(c) != std::string::npos && !negate) {
-                return true;
-            } else if (chars_to_match.find(c) == std::string::npos && negate) {
-                return true;
-            }
-        }
+    for (const auto &c : input_line) {
+      if (chars_to_match.find(c) != std::string::npos && !negate) {
+        return true;
+      } else if (chars_to_match.find(c) == std::string::npos && negate) {
+        return true;
+      }
     }
-    else {
-        throw std::runtime_error("Unhandled pattern " + pattern);
-    }
+    return false;
+  } else {
+    throw std::runtime_error("Unhandled pattern " + pattern);
+  }
 }
-
-
-int main(int argc, char* argv[]) {
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here" << std::endl;
-
-    if (argc != 3) {
-        std::cerr << "Expected two arguments" << std::endl;
-        return 1;
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "Expected two arguments" << std::endl;
+    return 1;
+  }
+  std::string flag = argv[1];
+  std::string pattern = argv[2];
+  if (flag != "-E") {
+    std::cerr << "Expected first argument to be '-E'" << std::endl;
+    return 1;
+  }
+  std::string input_line;
+  std::getline(std::cin, input_line);
+  try {
+    if (match_pattern(input_line, pattern)) {
+      return 0;
+    } else {
+      return 1;
     }
-
-    std::string flag = argv[1];
-    std::string pattern = argv[2];
-
-    if (flag != "-E") {
-        std::cerr << "Expected first argument to be '-E'" << std::endl;
-        return 1;
-    }
-
-    // Uncomment this block to pass the first stage
-    
-    std::string input_line;
-    std::getline(std::cin, input_line);
-    
-    try {
-        if (match_pattern(input_line, pattern)) {
-            return 0;
-        } else {
-            return 1;
-        }
-    } catch (const std::runtime_error& e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
+  } catch (const std::runtime_error &e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
 }
